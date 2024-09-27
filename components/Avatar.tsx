5aa0f3b8e6522a4c43643deab985f5343d8c559f
L00P3R93 <vincentkioko51@gmail.com>
2024-09-27 11:49:17 +0300

    Avatar Component

---
+++ components/Avatar.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, ViewStyle, TextStyle, Text, View } from 'react-native';
+
+interface Props {
+    initials: string;
+    size: number;
+    backgroundColor: string;
+}
+
+const Avatar: React.FC<Props> = ({ initials, size, backgroundColor }) => {
+    const style = {
+        width: size,
+        height: size,
+        borderRadius: size / 2,
+        backgroundColor
+    };
+
+    return (
+        <View style={[styles.container, style]}>
+            <Text style={styles.text}>{initials}</Text>
+        </View>
+    );
+};
+
+interface Style {
+    container: ViewStyle;
+    text: TextStyle;
+};
+
+const styles = StyleSheet.create<Style>({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+    }
+});
+
+export default Avatar;

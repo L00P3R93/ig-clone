dff35e62654d029f285bc231e7a96267145016d1
L00P3R93 <vincentkioko51@gmail.com>
2024-10-03 09:34:57 +0300

    CommentList.tsx Component

---
+++ components/CommentList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, View, ViewStyle } from 'react-native';
+
+interface Props {
+    items: string[]
+}
+
+const CommentList: React.FC<Props> = ({ items }) => {
+    const renderItem = (item: string, index: number) => (
+        <View key={index} style={styles.comment}>
+            <Text>{item}</Text>
+        </View>
+    );
+
+    return <ScrollView>{ items.map(renderItem) }</ScrollView>;
+}
+
+interface Style {
+    comment: ViewStyle;
+};
+
+const styles = StyleSheet.create<Style>({
+    comment: {
+        marginLeft: 20,
+        paddingVertical: 20,
+        paddingRight: 20,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        borderBottomColor: "rgba(0,0,0,0.05)"
+    }
+});
+
+export default CommentList;
